42cfbbc7b78c757e1d08969e90e0e72271a6ae5e
Maxx Kendy <83588041+Ken20085@users.noreply.github.com>
2026-06-17 01:23:51 +0800

    Infix to prefix work
    - CustomQueue now uses ArrayList
    - CustomStack now uses ArrayList
    - yes infix to prefix now works for mine (its probably the same solution anyway)
    - this can handle exponents and negative numbers
    - now to work on prefix to infix

---
+++ CustomStack.java
@@ -0,0 +1,118 @@
+/*
+ * This is a custom written stack data structure class.
+ *
+ * Note on Generics: This class uses Java Generics (<T>) backed by an Object array.
+ * Primitive types (like int, char) cannot be used. Use their respective
+ * java.lang equivalents (like Integer, Character).
+ *
+ * Reprogrammed to use ArrayList instead of array
+ *      Object[] stack --> ArrayList<Object> stack = new ArrayList<>();
+ *
+ * @author Johann Haree Tolentino
+ * @date 05/24/2026
+ * @modified 06/16/2026
+ * @param <T> The type of elements held in this stack
+ */
+
+import java.util.ArrayList;
+
+public class CustomStack<T> {
+    private final ArrayList<T> stack = new ArrayList<>();
+    private final int capacity;
+
+    public CustomStack(Integer maxSize)
+    {
+        if (maxSize == null || maxSize <= 0)
+        {
+            this.capacity = 10;
+        }
+        else
+        {
+            this.capacity = maxSize;
+        }
+    }
+
+    public CustomStack()
+    {
+        this.capacity = 10;
+    }
+
+    /**
+     * This function returns the last active element of the stack.
+     * Returns null if stack is empty or does not exist
+     * @return topmost element of type object
+     */
+    public T top()
+    {
+        if (isEmpty())
+        {
+            System.out.println("Stack is empty");
+            return null;
+        }
+
+        return (T) stack.getLast();
+    }
+
+    /**
+     * This function returns and removes the last active element of the stack.
+     * Returns null if stack is empty
+     * @return topmost element of type object
+     */
+    public T popItem()
+    {
+        if (isEmpty())
+        {
+            System.out.println("Stack is empty");
+            return null;
+        }
+
+        return stack.removeLast();
+    }
+
+    /**
+     * This function removes the last active element of the stack.
+     */
+    public void pop()
+    {
+        if (isEmpty())
+        {
+            System.out.println("Stack is empty");
+            return;
+        }
+
+        stack.removeLast();
+    }
+
+    /**
+     * This function adds a specified object type onto the top of the custom stack
+     * @param item object type
+     */
+    public void push(T item)
+    {
+        if (isFull())
+        {
+            System.out.println("Stack is full");
+            return;
+        }
+
+        this.stack.add(item);
+    }
+
+    /**
+     * This function returns true if the current count of custom stack is empty, false if otherwise
+     * @return boolean - True if count is 0, False if otherwise
+     */
+    public boolean isEmpty()
+    {
+        return stack.isEmpty();
+    }
+
+    /**
+     * This function returns true if the current count of custom stack is full, false if otherwise
+     * @return boolean - True if count is equal to set capacity, False if otherwise
+     */
+    public boolean isFull() {
+        return stack.size() == capacity;
+    }
+
+}
